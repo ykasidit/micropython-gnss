ff9036309c80d36ecd2b447b1a17f7693eddf4cd
Kasidit Yusuf <ykasidit@gmail.com>
2021-04-21 06:38:21 +0700

    using double float to handle gnss lat lon precision, as per https://github.com/micropython/micropython/issues/4380

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -41,7 +41,7 @@
 #define MICROPY_ENABLE_SOURCE_LINE          (1)
 #define MICROPY_ERROR_REPORTING             (MICROPY_ERROR_REPORTING_NORMAL)
 #define MICROPY_WARNINGS                    (1)
-#define MICROPY_FLOAT_IMPL                  (MICROPY_FLOAT_IMPL_FLOAT)
+#define MICROPY_FLOAT_IMPL                  (MICROPY_FLOAT_IMPL_DOUBLE)
 #define MICROPY_CPYTHON_COMPAT              (1)
 #define MICROPY_STREAMS_NON_BLOCK           (1)
 #define MICROPY_STREAMS_POSIX_API           (1)
